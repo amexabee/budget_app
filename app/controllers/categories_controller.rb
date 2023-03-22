c89a1f1c966daeb98a16c63aa683c44324f988df
Amanuel Galema <amexabee@yahoo.com>
2023-03-22 18:40:53 +0200

    Build basic form for adding category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,24 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'new', notice: 'not successful!'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon, :user_id)
+  end
 end
