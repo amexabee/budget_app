e4bcbb2506e53c12d1bc669b7e64b2a431e88fbb
Amanuel Galema <amexabee@yahoo.com>
2023-03-24 18:56:44 +0200

    Fix an edge case

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :groups, only: [:index, :new, :create] do
     resources :deals, only: [:index, :new, :create]
   end
-  devise_for :users
+  devise_for :users, controllers: { registrations: "custom_registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
